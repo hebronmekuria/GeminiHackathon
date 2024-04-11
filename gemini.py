678ecef188a102b641e9d34774bc098c6d26b296
hebronmekuria <hebron.mekuria@emu.edu>
2024-04-11 16:38:59 -0400

    Connected API Key to Gemini, produced results

---
+++ gemini.py
@@ -0,0 +1,15 @@
+import google.generativeai as genai
+
+#These imports are used to import the .env file which contains the API key
+from dotenv import load_dotenv
+import os
+
+#Fetch the API Key
+load_dotenv()
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("Give me ten names of cats")
+print(response.text)
