20079b159ac5f84177b57025a1615753f2746c64
hebronmekuria <hebron.mekuria@emu.edu>
2024-04-11 17:03:08 -0400

    Temperature and Limit Settings

--- gemini.py
+++ gemini.py
@@ -7,27 +7,55 @@ import os
 #Fetch the API Key
 load_dotenv()
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
-
 genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-pro')
+
 
 #Practice with a text prompt
+
+# model = genai.GenerativeModel('gemini-pro')
 # response = model.generate_content("Give me ten names of cats")
 # print(response.text)
 
 
 #Practice with an image + text prompt
-import PIL.Image
-img = PIL.Image.open('image.jpg')
-img
-prompt = """This image contains a sketch of a potential product along with some notes.
-Given the product sketch, describe the product as thoroughly as possible based on what you
-see in the image, making sure to note all of the product features. Return output in json format:
-{description: description, features: [feature1, feature2, feature3, etc]}"""
 
-model = genai.GenerativeModel('gemini-pro-vision') #different model from before
-response = model.generate_content([prompt, img]) #generate_content method takes multiple forms of inputs
-print(response.text)
+# import PIL.Image
+# img = PIL.Image.open('image.jpg')
+# img
+# prompt = """This image contains a sketch of a potential product along with some notes.
+# Given the product sketch, describe the product as thoroughly as possible based on what you
+# see in the image, making sure to note all of the product features. Return output in json format:
+# {description: description, features: [feature1, feature2, feature3, etc]}"""
+
+# model = genai.GenerativeModel('gemini-pro-vision') #different model from before
+# response = model.generate_content([prompt, img]) #generate_content method takes multiple forms of inputs
+# print(response.text)
 
 
+#Practice with chat history
+
+# model = genai.GenerativeModel('gemini-pro')
+# chat = model.start_chat(history=[])
+# response = chat.send_message("In one sentence, explain how a computer works to a young child.")
+# print(response.text)
+# response = chat.send_message("Okay, how about a more detailed explanation to a high schooler?")
+# print(response.text)
+# print(chat.history)
+
+#Temperature and Limit Setting for prompts
+#Higher Temperature- more creative responses
+
+# model = genai.GenerativeModel(
+#     'gemini-pro',
+#     generation_config=genai.GenerationConfig(
+#         max_output_tokens=2000,
+#         temperature=0.9, #Temp setting
+#     ))
+
+# response = model.generate_content(
+#     'Give me a numbered list of cat facts.',
+#     # Limit to 5 facts.
+#     generation_config = genai.GenerationConfig(stop_sequences=['\n6'])
+# )
+# print(response.text)
