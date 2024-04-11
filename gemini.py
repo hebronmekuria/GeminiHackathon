4231221605620ffacae44f1b0cf311e8fdf3daff
hebronmekuria <hebron.mekuria@emu.edu>
2024-04-11 16:52:03 -0400

    description added

--- gemini.py
+++ gemini.py
@@ -26,7 +26,7 @@ Given the product sketch, describe the product as thoroughly as possible based o
 see in the image, making sure to note all of the product features. Return output in json format:
 {description: description, features: [feature1, feature2, feature3, etc]}"""
 
-model = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-pro-vision') #different model from before
 response = model.generate_content([prompt, img]) #generate_content method takes multiple forms of inputs
 print(response.text)
 
