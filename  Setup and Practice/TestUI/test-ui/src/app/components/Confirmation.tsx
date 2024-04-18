f28e9bb11966a7b056c010dcc547f1622381931d
hebronmekuria <hebron.mekuria@emu.edu>
2024-04-17 22:37:42 -0400

    fix

---  Setup and Practice/TestUI/test-ui/src/app/components/Confirmation.tsx
+++  Setup and Practice/TestUI/test-ui/src/app/components/Confirmation.tsx
@@ -6,10 +6,8 @@ interface UploadConfirmationProps {
 
 const UploadConfirmation: React.FC<UploadConfirmationProps> = ({ message }) => {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
-      <div className="p-4 max-w-sm mx-auto bg-white rounded-lg shadow-xl text-center">
-        <p>{message}</p>
-      </div>
+    <div className="my-4 p-4 max-w-sm mx-auto bg-white rounded-lg shadow-xl text-center">
+      <p className="text-black">{message}</p>
     </div>
   );
 };
